bd6b0b3e27e549bce38073c48f045208664e43b3
wluhar <rahul1999engg@gmail.com>
2022-05-31 11:29:30 -0700

    Added Dad jokes api proj

---
+++ script.js
@@ -0,0 +1,33 @@
+const Jokes=document.querySelector('.jokes');
+const Jokesbtn=document.querySelector('.joke-btn');
+
+
+
+Jokesbtn.addEventListener('click',generateJokes);
+
+generateJokes()
+
+async function generateJokes(){
+    const config = {
+        headers : {
+            'Accept' : 'application/json'
+        }
+    }
+    const fetched=await fetch('https://icanhazdadjoke.com/',config)
+    const data=await fetched.json()
+    Jokes.innerText =data.joke ;
+}
+
+// function generateJokes(){
+//  const config={
+//      headers : {
+//          'Accept':'application/json'
+//      }
+//  }
+
+//     fetch('https://icanhazdadjoke.com',config)
+//     .then((response) => response.json())
+//     .then((data)=>{
+//         Jokes.innerText = data.joke;
+//     })
+// }
